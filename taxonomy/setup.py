dcec797f3a709b42b8382df650e66b0050417bb9
chrisjcameron <chrisjcameron@users.noreply.github.com>
2025-10-02 14:14:34 -0400

    add setup.py

---
+++ taxonomy/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='taxonomy',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='arXiv',
+    author_email='ccameron@arxiv.org',
+    description='A taxonomy utility package for arXiv metadata',
+    url='https://github.com/yourusername/taxonomy',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
